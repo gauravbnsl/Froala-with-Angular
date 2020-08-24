a7f9bf522e8241e59865b6fd6411d9d30c858a33
gauravbnsl <gauravbansal67@hotmail.com>
2020-08-24 12:23:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-froala-13aagf-bwejqk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-froala-13aagf-bwejqk)
---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  public model: any = {
+    details: 'empty'
+  };
+  name = 'Angular';
+  hide = false;
+
+  constructor(){
+    console.log('Ctor', this.model.details);
+  }
+
+  froalaOptions: Object = {
+    charCounterCount: false,
+    fileUpload: false,
+    attribution: false,
+    toolbarButtons: [
+      ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript'],
+      ['fontFamily', 'fontSize', 'backgroundColor', 'textColor'],
+      ['paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', '-', 'insertImage', 'embedly',
+        'insertTable', 'insertLink'],
+      ['specialCharacters', 'insertHR', 'clearFormatting'],
+      ['print', 'spellChecker'],
+      ['undo', 'redo']],
+    toolbarSticky: false,
+    language: 'de',
+    fontFamily: {
+      'Arial,Helvetica,sans-serif': 'Arial',
+      '\'Courier New\',Courier,monospace': 'Courier New',
+      'Georgia,serif': 'Georgia',
+      'Impact,Charcoal,sans-serif': 'Impact',
+      '\'Lucida Console\',Monaco,monospace': 'Lucida Console',
+      'Tahoma,Geneva,sans-serif': 'Tahoma',
+      '\'Times New Roman\',Times,serif': 'Times New Roman',
+      'Verdana,Geneva,sans-serif': 'Verdana',
+    },
+    events: {
+      'contentChanged': () => {
+        console.log('contentChanged', this.model.details);
+      }
+    },
+  };
+
+  newModel(){
+    this.model = {
+    };
+  }
+}
