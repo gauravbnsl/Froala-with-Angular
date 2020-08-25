ea808ca88bc8cada6304f2cacbdac048d4157a16
gauravbnsl <gauravbansal67@hotmail.com>
2020-08-25 11:18:33 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
   public model: any = {
-    details: 'empty'
+    details: "empty"
   };
   hide = false;
 
-  constructor(){
-    console.log('Ctor', this.model.details);
+  constructor() {
+    console.log("Ctor", this.model.details);
   }
 
   froalaOptions: Object = {
@@ -20,34 +20,86 @@ export class AppComponent  {
     fileUpload: false,
     attribution: false,
     toolbarButtons: [
-      ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript'],
-      ['fontFamily', 'fontSize', 'backgroundColor', 'textColor'],
-      ['paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', '-', 'insertImage', 'embedly',
-        'insertTable', 'insertLink'],
-      ['specialCharacters', 'insertHR', 'clearFormatting'],
-      ['print', 'spellChecker'],
-      ['undo', 'redo']],
+      [
+        "fullscreen",
+        "bold",
+        "italic",
+        "underline",
+        "strikeThrough",
+        "subscript",
+        "superscript"
+      ],
+      ["fontFamily", "fontSize", "backgroundColor", "textColor"],
+      [
+        "paragraphFormat",
+        "align",
+        "formatOL",
+        "formatUL",
+        "outdent",
+        "indent",
+        "-",
+        "insertImage",
+        "embedly",
+        "insertTable",
+        "insertLink"
+      ],
+      ["specialCharacters", "insertHR", "clearFormatting"],
+      ["print", "spellChecker"],
+      ["undo", "redo"],
+      ["insertImage"]
+    ],
     toolbarSticky: false,
-    language: 'de',
+    language: "de",
     fontFamily: {
-      'Arial,Helvetica,sans-serif': 'Arial',
-      '\'Courier New\',Courier,monospace': 'Courier New',
-      'Georgia,serif': 'Georgia',
-      'Impact,Charcoal,sans-serif': 'Impact',
-      '\'Lucida Console\',Monaco,monospace': 'Lucida Console',
-      'Tahoma,Geneva,sans-serif': 'Tahoma',
-      '\'Times New Roman\',Times,serif': 'Times New Roman',
-      'Verdana,Geneva,sans-serif': 'Verdana',
+      "Arial,Helvetica,sans-serif": "Arial",
+      "'Courier New',Courier,monospace": "Courier New",
+      "Georgia,serif": "Georgia",
+      "Impact,Charcoal,sans-serif": "Impact",
+      "'Lucida Console',Monaco,monospace": "Lucida Console",
+      "Tahoma,Geneva,sans-serif": "Tahoma",
+      "'Times New Roman',Times,serif": "Times New Roman",
+      "Verdana,Geneva,sans-serif": "Verdana"
     },
     events: {
-      'contentChanged': () => {
-        console.log('contentChanged', this.model.details);
+      contentChanged: () => {
+        console.log("contentChanged", this.model.details);
+      },
+      "element.dropped": function(element) {
+        // Do something here.
+        // this is the editor instance.
+        debugger;
+        console.log("works");
+      },
+      initialized: function (){
+        debugger;
+        console.log(this);
+        let editor = this.froalaOptions["events"];
+        // this.el.addEventListener('drop',this.froalaOptions["events"].drop(), true);
+        console.log("initialized", editor);
+        editor.drop(true);
+      },
+      'click': function (clickEvent) {
+      // Do something here.
+      // this is the editor instance.
+      debugger;
+      console.log(clickEvent);
+      },
+      'drop': function (dropEvent) {
+        // Do something here.
+        // this is the editor instance.
+        debugger;
+        console.log(dropEvent);
       }
-    },
+    }
   };
 
-  newModel(){
-    this.model = {
-    };
+  newModel() {
+    this.model = {};
+  }
+  addPlaceholder() {
+    console.log(this.model);
+  }
+  onDrop(event){
+    console.log('dropped');
   }
 }
